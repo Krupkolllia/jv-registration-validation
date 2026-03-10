daaace7b395a535b345c27345afcc3f3a8ffa48c
Krupkolllia <krupkoilla397@gmail.com>
2026-03-11 00:38:21 +0100

    feat: add custom InvalidRegisterArgumentException

---
+++ src/main/java/core/basesyntax/exception/InvalidRegisterArgumentException.java
@@ -0,0 +1,11 @@
+package core.basesyntax.exception;
+
+public class InvalidRegisterArgumentException extends Exception {
+    public InvalidRegisterArgumentException() {
+        super();
+    }
+
+    public InvalidRegisterArgumentException(String message) {
+        super(message);
+    }
+}
