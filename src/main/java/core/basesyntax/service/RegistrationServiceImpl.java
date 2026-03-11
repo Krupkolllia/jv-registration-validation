23be7d9a275c0cdb49be2868097200e13c5a55be
Krupkolllia <krupkoilla397@gmail.com>
2026-03-11 01:50:47 +0100

    feat: implement RegistrationService register()

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,37 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.InvalidRegisterArgumentException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
     private final StorageDao storageDao = new StorageDaoImpl();
 
+    public static final int MINIMUM_LOGIN_LENGTH = 6;
+    public static final int MINIMUM_PASSWORD_LENGTH = 6;
+    public static final int MINIMUM_AGE = 18;
+
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new InvalidRegisterArgumentException("User can't be null");
+        }
+
+        if (user.getLogin() == null || user.getLogin().length() < MINIMUM_LOGIN_LENGTH) {
+            throw new InvalidRegisterArgumentException(
+                    "Login does not exists or shorter than " + MINIMUM_LOGIN_LENGTH);
+        }
+
+        if (user.getPassword() == null || user.getPassword().length() < MINIMUM_PASSWORD_LENGTH) {
+            throw new InvalidRegisterArgumentException(
+                    "Password does not exists or shorter than " + MINIMUM_PASSWORD_LENGTH);
+        }
+
+        if (user.getAge() == null || user.getAge() < MINIMUM_AGE) {
+            throw new InvalidRegisterArgumentException("You must be older than " + MINIMUM_AGE);
+        }
+
+        storageDao.add(user);
+        return user;
     }
 }
